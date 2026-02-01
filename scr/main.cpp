efaa5a236f7bf9c25dda288069e1ca11d186c814
Dmitry-akva <errorsiniak@mail.ru>
2026-02-02 01:30:11 +0700

    Update main.cpp

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,1 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+    // Инициализация последовательного порта
+    Serial.begin(115200);
+    Serial.println("ESP8266 minimal build test");
+}
+
+void loop() {
+    // Просто мигаем встроенным светодиодом
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+}
 
