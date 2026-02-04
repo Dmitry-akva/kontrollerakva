87bc17252823ca1a1b5a0febf123813bed18629f
Dmitry-akva <errorsiniak@mail.ru>
2026-02-04 10:19:19 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
-#include <Arduino.h>
+#include <ESP8266mDNS.h> //для ота
+#include <WiFiUdp.h>   //для ота
+#include <ArduinoOTA.h>   //для ота
+#include <FastBot.h>   // телеграмм бот
+#include <OneWire.h>   // для датчика
+#include <DallasTemperature.h>  // для датчика
+#include <FileData.h>
+#include <LittleFS.h>
+#include <GyverPortal.h>
+#include <GyverHC595.h>
+#include <EEPROM.h>
+#include <GTimer.h>
 
 void setup() {
     // Инициализация последовательного порта
